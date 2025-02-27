a39f3a146a7dab5e04317e29b2db96f6d26ee942
Giovani Sanfer <143708066+sanfergio@users.noreply.github.com>
2025-02-27 21:41:24 +0000

    Carrousel com sevirços e adição dos area dos clientes

--- FE-COMPONENTS/Animations.js
+++ FE-COMPONENTS/Animations.js
@@ -1,3 +1,5 @@
+// função para ativar fade-in 
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -14,3 +16,31 @@ document.querySelectorAll(".fade-in").forEach((element) => {
 document.querySelectorAll(".fade-in-img").forEach((element) => {
     observer.observe(element);
 });
+
+// funcão carrousel dos serviços
+
+const CarouselService = document.querySelector(".servicos-pai");
+let isAuto = true;
+
+function stopAuto() {
+    if (isAuto) {
+        CarouselService.style.animation = 'none';
+        isAuto = false;
+    }
+}
+
+function direita() {
+    stopAuto();
+    CarouselService.style.animation = 'none';
+    void CarouselService.offsetWidth;
+    CarouselService.style.animation = 'moveLeftRight 2s forwards';
+}
+
+function esquerda() {
+    stopAuto();
+    CarouselService.style.animation = 'none';
+    void CarouselService.offsetWidth;
+    CarouselService.style.animation = 'moveRightLeft 2s forwards';
+}
+
+CarouselService.addEventListener('click', stopAuto);
