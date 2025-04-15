cb8f09daf9f059603086da4a2843e2522b27e29a
Giovani Sanfer <143708066+sanfergio@users.noreply.github.com>
2025-04-15 14:07:03 +0000

    Mudando chamada de JS e corrigindo Link do Servicos

--- FE-COMPONENTS/Menu.js
+++ FE-COMPONENTS/Menu.js
@@ -35,7 +35,7 @@ Menu.innerHTML = `
                     <i style="overflow-y: hidden; margin-left: 5vw;"  class="fa fa-close"></i>
                 </div>
                 <a href="/Sobre/" class="menu-line">Sobre nós</a>
-                <a href="/Manutencao/" class="menu-line">Serviços de Manunteção</a>
+                <a href="/Servicos/" class="menu-line">Serviços</a>
                 <a href="/Cases/" class="menu-line">Cases de Sucesso</a>
                 <a href="https://ead.fabricaescondida.com.br" target = "_blank" class="menu-line">Fábrica Escondida EAD</a>
                 <a href="https://api.whatsapp.com/send/?phone=5511994439172&text=Ol%C3%A1%21+Vim+pelo+website+e+desejo+tirar+d%C3%BAvidas.&type=phone_number&app_absent=0" target = "_blank" class="menu-line">Fale Conosco</a>                
