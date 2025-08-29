a199929332aa1d32755391735a6447b52eeea89d
Giovani Sanfer <giovani.marlonfer@gmail.com>
2025-08-29 15:32:47 -0300

    add timeline page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home"; // <--- importando o novo componente
+import NossaHistoria from "./components/NossaHistoria";
 
 function App() {
   return (
     <div>
       {/* Navbar */}
       <Header />
-
+      
       {/* Home */}
-      <Home />
+      <NossaHistoria />
     </div>
   );
 }
